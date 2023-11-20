0f7a66dad9c3a90f13ef24c81520a5b5ee0f2631
me <me@gmail.com>
2023-11-20 17:23:53 +0100

    first commit: HTTPS Protocol with SSL Certificate

---
+++ server.js
@@ -0,0 +1,23 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secrets', (req, res) => {
+    return res.send('Your personal secret value is 42')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+https.createServer({
+    key:fs.readFileSync('key.pem'),
+    cert:fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+});
