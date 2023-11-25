67772af498ba97eaa92f41b4e664f2d625ad9776
me <me@gmail.com>
2023-11-25 11:27:11 +0100

    sixth commit: Reading and Writing Oauth Session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const helmet = require('helmet');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 const { Strategy } = require('passport-google-oauth20');
 
@@ -16,7 +17,9 @@ const PORT = 3000;
 //Dotenv Variables
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 //Passport Strategy (for Google)
@@ -31,13 +34,33 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 };
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
+//Serializing: Save the session to the cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+//Deserializing: Read the session from the cookie
+passport.deserializeUser((id, done) => {
+    done(null, id);
+});
+
 const app = express();
 
 //Security Related Middleware
 app.use(helmet());
 
-//Passport Middleware
+//Cookie Session Middleware
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}));
+
+//Passport Inizialize Middleware
 app.use(passport.initialize());
+
+//Passport Session Middleware
+app.use(passport.session());
 
 //OAuth Middleware
 function checkLoggedIn (req, res, next) {
@@ -57,7 +80,7 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback', passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false,
+    session: true, //True if cookie-session is configured
 }), (req, res) => {
     console.log('Google called us back!')
 });
